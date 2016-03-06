dc0dfe5f31ab0fc8df5e208014e02f550d7058d3
mezuky04 <alexandru.bugarin@gmail.com>
2016-03-06 14:20:37 +0200

    Style, comment and organize code

--- header.php
+++ header.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-    
-    <head>
-        <meta charset="<?php bloginfo('charset'); ?>">
-        <meta name="viewport" content="width=device-width">
-        <title><?php bloginfo('name'); ?></title>
-        <?php wp_head(); ?>
-    </head>
 
-    <body <?php body_class(); ?>>
+<!-- BEGIN Head -->
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width">
+    <title><?php bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
+</head>
+<!-- END Head -->
+
+<!-- BEGIN Body -->
+<body <?php body_class(); ?>>
 
     <!-- BEGIN Fluid container -->
     <div class="container-fluid">
@@ -16,14 +19,21 @@
         <!-- BEGIN Header -->
         <header class="site-header">
 
+            <!-- BEGIN Navigation bar -->
             <div class="navbar navbar-default navbar-fixed-top <?php if(is_user_logged_in()):?>margin-top<?php endif?>" role="navigation">
+
+                <!-- BEGIN Navigation bar content -->
                 <div class="container">
+
+                    <!-- BEGIN Theme title -->
                     <div class="navbar-header">
                         <a class="navbar-brand" href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
                     </div>
+                    <!-- END Theme title -->
+
+                    <!-- BEGIN Navigation menu -->
                     <div class="navbar-collapse collapse pull-right navigation-bar">
-                        <?php
-                        wp_nav_menu( array(
+                        <?php wp_nav_menu([
                                 'menu'              => 'primary',
                                 'theme_location'    => 'primary',
                                 'depth'             => 2,
@@ -32,12 +42,16 @@
                                 'container_id'      => 'bs-example-navbar-collapse-1',
                                 'menu_class'        => 'nav navbar-nav',
                                 'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-                                'walker'            => new wp_bootstrap_navwalker())
-                        );
-                        ?>
-                    </div><!--/.nav-collapse -->
-                </div><!--/.container -->
+                                'walker'            => new wp_bootstrap_navwalker()
+                        ]); ?>
+                    </div>
+                    <!-- END Navigation menu -->
+
+                </div>
+                <!-- END Navigation bar content -->
+
             </div>
+            <!-- END Navigation bar -->
 
         </header>
         <!-- END Header -->
@@ -45,11 +59,13 @@
     </div>
     <!-- END Fluid container -->
 
+    <!-- BEGIN Sub header -->
     <div class="sub-header">
         <div class="container">
             <h3>Școala dedicată tehnologiei!</h3>
         </div>
     </div>
+    <!-- END Sub header -->
 
     <!-- BEGIN Container -->
     <div class="container margin-top">
