c7b757ce2ed90bf4cbbde23b11a1dd4601051b4b
mezuky04 <alexandru.bugarin@gmail.com>
2016-03-06 14:22:17 +0200

    Style, comment and organize code

--- footer.php
+++ footer.php
@@ -1,11 +1,23 @@
+    <!-- BEGIN Footer -->
     <footer class="site-footer col-md-12 text-center">
+        <!-- BEGIN Copyright -->
         <p><?php bloginfo('name'); ?> - &copy; <?php echo date('Y'); ?>. Un produs <a href="#">Bitller</a></p>
-        <div><img src="<?php echo get_site_url(); ?>/wp-content/themes/techno/facebook.png"></div>
+        <!-- END Copyright -->
+
+        <!-- BEGIN View on facebook -->
+        <div>
+            <img src="<?php echo get_site_url(); ?>/wp-content/themes/techno/facebook.png">
+        </div>
+        <!-- END View on facebook -->
+        
     </footer>
+    <!-- END Footer -->
 
 </div>
 <!-- END Container -->
 
 <?php wp_footer(); ?>
 </body>
+<!-- END Body -->
+
 </html>
